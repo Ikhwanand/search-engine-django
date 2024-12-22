bc69ce3aa90c151268fe043f5a067b138b1b481f
ikhwanand <aldirizaldy977@gmail.com>
2024-12-22 20:18:15 +0800

    updated

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ pip install -r requirements.txt
 # Run Django migrations
 python manage.py migrate
 
-# Collect static files if needed (optional)
-python manage.py collectstatic --noinput
 
 # Build your Django app (if needed, e.g., using a bundler or custom build process)
 python manage.py tailwind start
