1bee1332d5e5d8952ef6f0cf564a2384f24d460c
ikhwanand <aldirizaldy977@gmail.com>
2024-12-22 20:51:18 +0800

    updated

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+python3 -m venv venv
+
+source ./venv/bin/activate
+
 # Install Python dependencies
 pip3 install -r requirements.txt
+
+pip3 install -U pip
 
 # Install Node.js dependencies
 cd theme/static_src/ 
@@ -12,10 +18,12 @@ cd ..
 
 cd ..
 
+python3 manage.py makemigrations
+
 # Run Django migrations
 python3 manage.py migrate
 
 python3 manage.py tailwind start
 
 # command to open another new terminal 
-gnome-terminal --tab -- bash -c "python manage.py runserver; exec bash"
+gnome-terminal --tab -- bash -c "python3 manage.py runserver; exec bash"
